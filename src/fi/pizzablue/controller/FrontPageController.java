4f4f020deb807d94f13c3ba4544beca4ccfc4891
Susanna Hyötyläinen <a1402957@myy.haaga-helia.fi>
2015-10-02 13:22:08 +0300

    projekti siirretty serverille ja eclipse workspaceen

---
+++ src/fi/pizzablue/controller/FrontPageController.java
@@ -0,0 +1,26 @@
+package fi.pizzablue.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PizzaListController
+ */
+@WebServlet("/index.jsp")
+public class FrontPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO get list from db
+		request.getRequestDispatcher("WEB-INF/jsp/frontpage.jsp").forward(request, response);
+	}
+
+}
