17a38544b2e974f8a55c6f6eec6a740cf7e11006
Mimosa Heino <a1402990@myy.haaga-helia.fi>
2015-10-26 14:50:53 +0200

    juomat haetaan tietokannasta listalle, luotu juomaDAO ja juomalistaService. Tuotu myös juomien kuvat sivulle. Vaihdettu valitse -napin teksti lisää ostoskoriin -tekstiksi

---
+++ src/fi/pizzablue/service/JuomalistaService.java
@@ -0,0 +1,26 @@
+package fi.pizzablue.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import fi.pizzablue.bean.Juoma;
+import fi.pizzablue.dao.DAOPoikkeus;
+import fi.pizzablue.dao.JuomaDAO;
+import fi.pizzablue.dao.Yhteys;
+
+public class JuomalistaService {
+
+	JuomaDAO jDAO = new JuomaDAO();
+	
+	public List<Juoma> haeJuomalista() throws DAOPoikkeus {
+
+		Connection yhteys = Yhteys.avaaYhteys();
+		
+		List<Juoma> juomat = jDAO.haeJuomat(yhteys);
+		
+		Yhteys.suljeYhteys(yhteys);
+		return juomat;
+		
+	}
+
+}
