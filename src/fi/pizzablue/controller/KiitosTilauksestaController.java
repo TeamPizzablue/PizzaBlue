e8ff242d7165bb70673c370cc8b0deac72bc876f
Mimosa Heino <a1402990@myy.haaga-helia.fi>
2015-12-11 12:05:55 +0200

    yhteishintaan lisätään kotiinkuljetushinta, tiedostojen nimiä muutettu

--- src/fi/pizzablue/controller/KiitosTilauksestaController.java
+++ src/fi/pizzablue/controller/KiitosTilauksestaController.java
@@ -7,23 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TilaussivuController
- */
-@WebServlet(urlPatterns={"/toimitus", "/toimitustiedot.jsp"})
-public class ToimitustiedotController extends HttpServlet {
+@WebServlet("/kiitostilauksesta")
+public class KiitosTilauksestaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ToimitustiedotController() {
+
+    public KiitosTilauksestaController() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.getRequestDispatcher("WEB-INF/jsp/toimitustiedot.jsp").forward(request, response);
+		request.getRequestDispatcher("WEB-INF/jsp/kiitosTilauksesta.jsp").forward(request, response);
 		
 	}
+
 }
